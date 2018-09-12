86653bfe122d3993b057b3b0703ec7b8e98edcd2
shouwn <sjonghyon@gmail.com>
2018-09-12 18:30:31 +0900

    액션 바 추가

--- app/src/main/java/net/skhu/e01button/MainActivity.kt
+++ app/src/main/java/net/skhu/e01button/MainActivity.kt
@@ -3,10 +3,14 @@ package net.skhu.e01button
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import net.skhu.e01button.R.styleable.View
 
 class MainActivity : AppCompatActivity() {
@@ -18,15 +22,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate 메소드 시작")
-
-        this.findViewById<Button>(R.id.button).setOnClickListener {
-            Log.d(TAG, "onClick 메소드 시작")
-            val text1 = this.findViewById<EditText>(R.id.editText).text
-            this.findViewById<EditText>(R.id.editText).text = this.findViewById<EditText>(R.id.editText2).text
-            this.findViewById<EditText>(R.id.editText2).text = text1
-            Log.d(TAG, "onClick 메소드 끝")
-        }
-        Log.d(TAG, "onCreate 메소드 끝")
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean =
+        when(item?.itemId){
+            R.id.action_signUp -> {
+                Toast.makeText(this, "회원가입 메뉴 클릭", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_memo -> {
+                Toast.makeText(this, "메모장 메뉴 클릭", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
 }
